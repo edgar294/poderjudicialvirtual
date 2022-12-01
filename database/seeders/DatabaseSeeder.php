6d090b6e79efa95a1d17a25ca816c5c1a6feb809
Edgar Rojas <edgarrojas2904@gmail.com>
2022-12-01 11:21:20 -0400

    Change administrator mail and README.md

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         // Crear usuario base
         $user = User::create([
             'name' => "Edgar Rojas",
-            'email' => "edgarrojas2904@gmail.com",
+            'email' => "edgar@correo.com",
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
