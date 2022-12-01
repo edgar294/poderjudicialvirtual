e9912177eb64fdf2d8da9985d49fe413a62bbbf9
Edgar Rojas <edgarrojas2904@gmail.com>
2022-12-01 09:20:19 -0400

    Add procces to buy a product

--- app/Http/Controllers/CompraController.php
+++ app/Http/Controllers/CompraController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Compra;
+use App\Models\Producto;
 
 class CompraController extends Controller
 {
-    //
+    public function buyProduct(Request $request, Producto $producto)
+    {
+        $user = auth()->user();
+        $price = $producto->precio;
+        $tax = $producto->impuesto;
+
+        Compra::create([
+            'user_id' => $user->id,
+            'producto_id' => $producto->id,
+            'precio' => $producto->precio,
+            'impuesto' => $producto->impuesto
+        ]);
+
+        return redirect(route('compras.index'));
+    }
 }
